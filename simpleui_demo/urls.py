a9b6cbdae021fb6d2d7ec7a723c8cc1f50a1d670
2391531563@qq.com <2391531563@qq.com>
2020-12-07 22:42:32 +0800

    修改系统名称

--- simpleui_demo/urls.py
+++ simpleui_demo/urls.py
@@ -19,8 +19,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.site_title = '管理后台'
-admin.site.site_header = '员工管理后台'
+admin.site.site_title = 'SRS'
+admin.site.site_header = '智慧零售系统'
 
 urlpatterns = [
                   # 配置admindoc
